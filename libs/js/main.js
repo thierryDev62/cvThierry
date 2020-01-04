cb9b71c2eafb03b5b6888abb47a4727eb26785dd
thierryDev62 <56256915+thierryDev62@users.noreply.github.com>
2020-01-04 14:31:03 +0100

    update

--- libs/js/main.js
+++ libs/js/main.js
@@ -1,4 +1,14 @@
-$(function() {
+$(function () {
     $("body").css("font-family", "'Roboto', Arial, sans-serif");
+
+    $("#corps").load("templates/competences.html");
+
+    $("#boutonCompetences").on("click", function () {
+        $("#corps").load("templates/competences.html");
+    });
+
+    $("#boutonExperiencesPro").on("click", function() {
+        $("#corps").load("templates/experiences.html")
+    })
 })
 
