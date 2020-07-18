f187ba4a6f6599f6dd4124f66ab2d53023eade97
thierrydev62 <thierrydev62@gmail.com>
2020-07-18 17:44:03 +0200

    update 18-07-2020 17:43

--- libs/js/main.js
+++ libs/js/main.js
@@ -1,29 +1,37 @@
 $(function () {
-    $("body").css("font-family", "'Sriracha', cursive, Arial, sans-serif");
+    let button = $("button");
+    let logo = $("#logo");
+    let corps = $("#corps");
+    let boutonCompetences = $("#boutonCompetences");
+    let boutonExperiencesPro = $("#boutonExperiencesPro");
+    let boutonFormation = $("#boutonFormation");
+    let boutonRealisations = $("#boutonRealisations");
+    let boutonLesPlus = $("#boutonLesPlus");
 
-    $("#corps").load("templates/competences.html");
+    corps.load("templates/competences.html");
 
     // Animation des boutons du menu
 
-    $("button").on("mouseover", function () {
+    button.on("mouseover", function () {
         $(this).css({
             "transform": "scale(1.1)",
             "transition-property": "transform",
             "transition-duration": "0.2s",
             "border-radius": "25% 10%",
             "box-shadow": "0 2px 8px rgba(0,0,0,0.4), 0 1px 9px rgba(0,0,0,0.2)"
-        });
+        }).addClass("border border-warning");
     });
-    $("button").on("mouseout", function () {
+
+    button.on("mouseout", function () {
         $(this).css({
             "transform": "scale(1)",
             "border-radius": "0",
             "box-shadow": "none"
-        });
+        }).removeClass("border border-warning");
     });
 
     // La photo de profil
-    $("#logo").on("mouseenter", function(){
+    logo.on("mouseenter", function(){
         $(this).css(
             {
                 "transform": "scale(1.1)",
@@ -33,7 +41,7 @@ $(function () {
             }
         )
     });
-    $("#logo").on("mouseleave", function(){
+    logo.on("mouseleave", function(){
         $(this).css(
             {
                 "transform": "scale(1)",
@@ -44,25 +52,24 @@ $(function () {
 
     // Partie avec tous les évènements aux clics dans les différentes rubriques
 
-    $("#boutonCompetences").on("click", function () {
-        $("#corps").load("templates/competences.html").hide().fadeIn();
+    boutonCompetences.on("click", function () {
+        corps.load("templates/competences.html").hide().fadeIn();
     });
 
-    $("#boutonExperiencesPro").on("click", function () {
-        $("#corps").load("templates/experiences.html").hide().fadeIn();
+    boutonExperiencesPro.on("click", function () {
+        corps.load("templates/experiences.html").hide().fadeIn();
     });
 
-    $("#boutonFormation").on("click", function () {
-        $("#corps").load("templates/formation.html").hide().fadeIn();
+    boutonFormation.on("click", function () {
+        corps.load("templates/formation.html").hide().fadeIn();
     });
 
-    $("#boutonRealisations").on("click", function () {
-        $("#corps").load("templates/realisations.html").hide().fadeIn();
+    boutonRealisations.on("click", function () {
+        corps.load("templates/realisations.html").hide().fadeIn();
     });
 
-    $("#boutonLesPlus").on("click", function () {
-        $("#corps").load("templates/lesplus.html").hide().fadeIn();
+    boutonLesPlus.on("click", function () {
+        corps.load("templates/lesplus.html").hide().fadeIn();
     });
-    
-})
+});
 
